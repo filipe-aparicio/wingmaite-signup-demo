f037d843eec0e868abe0527fb84359b69da16a89
filipe-aparicio <filipe.aparicio91@gmail.com>
2026-02-18 01:55:18 +0000

    feature: add navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Navbar from '@/app/components/navbar/navbar';
 import Button from '@/app/components/ui/button';
 import { LogIn } from 'lucide-react';
 
 export default function Page() {
   return (
     <div className="bg-pine-50 w-screen h-screen flex items-center justify-center">
+      <Navbar />
       <Button variant="primary" href="/signup">
         <LogIn size={16} /> Log in
       </Button>
